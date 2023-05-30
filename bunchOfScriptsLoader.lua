a4cbe5a02857aaf5ee68aeae4a587a813464702b
diepedyt <106111219+diepedyt@users.noreply.github.com>
2023-05-30 22:23:53 +0100

    Add files via upload

---
+++ bunchOfScriptsLoader.lua
@@ -0,0 +1,13 @@
+repeat task.wait() until game:IsLoaded()
+repeat task.wait() until game:IsLoaded()
+
+_G.Scripts = {
+    o = "bkjscajksadnj",
+    d = "j58UXW2wQh",
+    [8720980067] = {
+        ["Script 1"] = "good script"
+    }
+
+}
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/diepedyt/freeScriptLoader/main/safe.lua'))()
