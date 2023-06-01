9650958eaae6b8044735d3e738692acbb1585f01
diepedyt <106111219+diepedyt@users.noreply.github.com>
2023-06-01 22:28:18 +0100

    Update bunchOfScriptsLoader.lua

--- bunchOfScriptsLoader.lua
+++ bunchOfScriptsLoader.lua
@@ -5,8 +5,16 @@ _G.Scripts = {
     d = "j58UXW2wQh",
     [5956785391] = {
         ["V$L Valynium"] = "https://raw.githubusercontent.com/XO-3S-CL-VCK-jf-3HDM/Products/main/Project-Slayers.lua"
+    },
+    [2753915549] = {
+        ["Mukuro Hub - No Key System"] = "https://raw.githubusercontent.com/xQuartyx/DonateMe/main/ScriptLoader"
+    },
+    [5938036553] = {
+        ["ESP - And More"] = "https://pastebin.com/raw/d3ZbwBei"
+    },
+    [1554960397] = {
+        ["Script 1"] = "https://raw.githubusercontent.com/03sAlt/BlueLockSeason2/main/README.md"
     }
-
 }
 
 loadstring(game:HttpGet('https://raw.githubusercontent.com/diepedyt/freeScriptLoader/main/safe.lua'))()
