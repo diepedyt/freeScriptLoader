5a91346919d15dbf84a452aafb5659d0a7d4061e
diepedyt <106111219+diepedyt@users.noreply.github.com>
2023-11-07 14:18:06 +0000

    Update safe.lua

--- safe.lua
+++ safe.lua
@@ -10,5 +10,5 @@ _G.KeyInserted = ui.CreateKeySystem("link-hub.net/977929/script",nil,"Hub", "INP
 repeat task.wait() until _G.INPUT == _G.Scripts.o:reverse():lower()
 _G.DESTROYNOW = true
 
-loadstring(game:HttpGet(_G.Scripts[game.GameId]]1]))()
+loadstring(game:HttpGet(_G.Scripts[game.GameId][1]))()
 
