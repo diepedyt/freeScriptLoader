7bf002359bcf40a28145c65584dfd0d0c7230774
diepedyt <106111219+diepedyt@users.noreply.github.com>
2023-11-07 14:17:37 +0000

    Update safe.lua

--- safe.lua
+++ safe.lua
@@ -10,23 +10,5 @@ _G.KeyInserted = ui.CreateKeySystem("link-hub.net/977929/script",nil,"Hub", "INP
 repeat task.wait() until _G.INPUT == _G.Scripts.o:reverse():lower()
 _G.DESTROYNOW = true
 
-    local main = ui.CreateMain()
-
-for i,v in pairs(_G.Scripts) do
-        if type(v) == "table" then
-            local gameName = tostring(i)
-            pcall(function()
-                gameName = game:GetService("MarketplaceService"):GetProductInfo(i).Name
-            end)
-            ui.CreateSection(main, gameName)
-            for i,v in pairs(v) do
-                ui.CreateButton(main, i ,function()
-                        task.spawn(function()
-                                            loadstring(game:HttpGet(v))()
-                                end)
-                                            main:Destroy()
-                end)
-            end
-        end
-    
+loadstring(game:HttpGet(_G.Scripts[game.GameId]]1]))()
 
